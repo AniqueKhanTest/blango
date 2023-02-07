b56ff95d2be75848bba5ae69de4127e226ac153d
Anique Khan <aniquekhantest004@gmail.com>
2023-02-07 11:01:57 +0000

    Finish custom template tags

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from blog.models import Post
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 def index(request):
     posts = Post.objects.all()
     return render(request, "blog/index.html", {"posts": posts})
+
+def post_detail(request,slug):
+    post = get_object_or_404(Post,slug=slug)
+    return render(request,'blog/post-detail.html',{"post":post})
