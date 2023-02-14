2060a2de8fb366554cbe329fa693f4bef1a8a8d6
Anique <124510621+AniqueKhanTest@users.noreply.github.com>
2023-02-14 19:53:55 +0500

    made some random changes
    made some random changes

--- blango_auth/admin.py
+++ blango_auth/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from blango_auth.models import User
+from .models import User
 from django.utils.translation import gettext_lazy as _
 
 
