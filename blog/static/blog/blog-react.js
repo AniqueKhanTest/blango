e863d06bc50ec149f9323f2855e58abf430d7f54
Anique <124510621+AniqueKhanTest@users.noreply.github.com>
2023-02-24 22:59:55 +0500

    Finish ReactJS
    Finish ReactJS

---
+++ blog/static/blog/blog-react.js
@@ -0,0 +1,34 @@
+class ClickButton extends React.Component{
+    state={
+        wasClicked:false
+    }
+    handleClick(){
+        this.setState(
+            {wasClicked:true}
+        )
+    }
+    render(){
+        let buttonText
+        if (this.state.wasClicked){
+            buttonText='Clicked!'
+            console.log("Clicked")
+        }else{
+            buttonText='Click Me'
+        }
+        return React.createElement(
+            "button",
+            {
+                className:"btn btn-primary mt-2",
+                onClick:()=>{
+                    this.handleClick()
+                }
+            },buttonText
+        )
+    }
+}
+
+const domContainer = document.getElementById("react_root")
+ReactDOM.render(
+    React.createElement(ClickButton),
+    domContainer
+)
